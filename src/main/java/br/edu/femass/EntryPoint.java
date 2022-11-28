9b8710f6ec6388ca8b031c31a08da8870b85e866
Davi Trajano <davitrajano473@gmail.com>
2022-11-28 01:38:58 -0300

    novo

--- src/main/java/br/edu/femass/EntryPoint.java
+++ src/main/java/br/edu/femass/EntryPoint.java
@@ -12,7 +12,7 @@ public class EntryPoint extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/GuiLivro.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainBibliotecario.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
         scene.getRoot().setStyle("-fx-font-family: 'serif'");
